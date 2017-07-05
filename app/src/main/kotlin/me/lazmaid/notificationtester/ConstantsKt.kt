b34a8d2f6e1b0cfcf9c0e75d42d2eb28345d200b
Verachad Wongsawangtham <birth24987@gmail.com>
2017-07-05 23:54:38 +0700

    Add MainAcitivtyKt

---
+++ app/src/main/kotlin/me/lazmaid/notificationtester/ConstantsKt.kt
@@ -0,0 +1,8 @@
+package me.lazmaid.notificationtester
+
+/**
+ * Created by vwongsawangt on 7/5/2017 AD.
+ */
+class ConstantsKt {
+
+}
---
+++ app/src/main/kotlin/me/lazmaid/notificationtester/NotificationBoardcastReceiverKt.kt
@@ -0,0 +1,15 @@
+package me.lazmaid.notificationtester
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+/**
+ * Created by vwongsawangt on 7/5/2017 AD.
+ */
+class NotificationBoardcastReceiverKt : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent?) {
+
+    }
+}
